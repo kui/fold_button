90c4387d63d2ba6c815dd0e1bfedc58847d96a94
Keiichiro Ui <keiichiro.ui@gmail.com>
2014-08-13 04:24:06 +0900

    Fix for dart2js on chrome

--- lib/fold_button.dart
+++ lib/fold_button.dart
@@ -1,13 +1,15 @@
 library fold_button;
 
 import 'dart:html';
+import 'dart:async';
 import 'package:polymer/polymer.dart';
 
 @CustomTag('fold-button')
 class FoldButtonElement extends PolymerElement {
-  static const TRANSITION_DURATION = '0.2s';
   static const List<String> PROP_NAMES =
       const ['display', 'transform', 'opacity', 'transform-origin', 'transition'];
+  static const TRANSITION_DURATION = const Duration(milliseconds: 200);
+  static const DISPLAY_DELAY = const Duration(milliseconds: 80);
 
   @PublishedProperty(reflect: true)
   String get target => readValue(#target);
@@ -24,6 +26,7 @@ class FoldButtonElement extends PolymerElement {
   HtmlElement get _targetElement =>
       target == null ? null : querySelector(target);
   CssStyleDeclaration _originalStyle;
+  Timer _transitionEndTimer;
 
   FoldButtonElement.created() : super.created();
 
@@ -31,7 +34,6 @@ class FoldButtonElement extends PolymerElement {
   attached() {
     super.attached();
     onClick.listen((_) => toggle());
-    _targetElement.onTransitionEnd.listen(handleTransitionEnd);
   }
 
   void toggle() { folding = !folding; }
@@ -61,14 +63,19 @@ class FoldButtonElement extends PolymerElement {
       return;
     }
 
-    _saveStyles(t.style);
+    if (_transitionEndTimer == null) {
+      _saveStyles(t.style);
+      final Point<double> delta = _getCenterPoint(this) - _getTopLeftPoint(t);
+      _setStyle(t.style, 'transform-origin',
+          '${delta.x.round()}px ${delta.y.round()}px');
+    }
 
-    t.style.transform = 'scale(0)';
-    t.style.opacity = '0';
-    t.style.transition = 'transform $TRANSITION_DURATION ease-in, '
-        'opacity $TRANSITION_DURATION ease-out';
-    final Point<double> delta = getCenterPoint(this) - getTopLeftPoint(t);
-    t.style.transformOrigin = '${delta.x.round()}px ${delta.y.round()}px';
+    _setStyle(t.style, 'transform', 'scale(0)');
+    _setStyle(t.style, 'opacity', '0');
+    _setStyle(t.style, 'transition',
+        'transform ${TRANSITION_DURATION.inMilliseconds}ms ease-in  0s, '
+        'opacity   ${TRANSITION_DURATION.inMilliseconds}ms ease-out 0s');
+    startTransitionEndTimer();
   }
 
   void _unfoldTarget() {
@@ -80,13 +87,21 @@ class FoldButtonElement extends PolymerElement {
 
     _restoreStyle(t.style, 'display');
 
-    async((_) {
-      _restoreStyle(t.style, 'transform');
-      _restoreStyle(t.style, 'opacity');
+    new Timer(DISPLAY_DELAY, () {
+      _restoreStyle(t.style, 'transform', 'scale(1)');
+      _restoreStyle(t.style, 'opacity', '1');
+      startTransitionEndTimer();
     });
   }
 
-  void handleTransitionEnd(TransitionEvent event) {
+  void startTransitionEndTimer() {
+    if (_transitionEndTimer != null) {
+      _transitionEndTimer.cancel();
+    }
+    _transitionEndTimer = new Timer(TRANSITION_DURATION, handleTransitionEnd);
+  }
+
+  void handleTransitionEnd() {
     final t = _targetElement;
     if (t == null) return;
 
@@ -95,8 +110,13 @@ class FoldButtonElement extends PolymerElement {
     } else {
       _restoreStyles(t.style);
     }
+
+    _transitionEndTimer = null;
   }
 
+  _setStyle(CssStyleDeclaration style, String propName, String value) =>
+      _listWithVenderPrefix(propName).forEach((name) {
+          style.setProperty(name, value); print(name);});
   _saveStyles(CssStyleDeclaration style) =>
       PROP_NAMES.forEach((p) => _saveStyle(style, p));
   _saveStyle(CssStyleDeclaration style, String propName) {
@@ -105,32 +125,30 @@ class FoldButtonElement extends PolymerElement {
   }
   _restoreStyles(CssStyleDeclaration style) =>
       PROP_NAMES.forEach((p) => _restoreStyle(style, p));
-  _restoreStyle(CssStyleDeclaration style, String propName) {
+  _restoreStyle(CssStyleDeclaration style, String propName, [String defaultValue]) {
     if (_originalStyle == null) return;
     final p = _originalStyle.getPropertyValue(propName);
-    if (p == null) {
-      style.removeProperty(propName);
-      style.removeProperty('-webkit-$propName');
-      style.removeProperty('-moz-$propName');
-      style.removeProperty('-ms-$propName');
-      style.removeProperty('-o-$propName');
+    final value = (p == null) ? defaultValue : p;
+    if (value == null) {
+      _listWithVenderPrefix(propName).forEach(style.removeProperty);
     } else {
-      style.setProperty(propName, p);
-      style.setProperty('-webkit-$propName', p);
-      style.setProperty('-moz-$propName', p);
-      style.setProperty('-ms-$propName', p);
-      style.setProperty('-o-$propName', p);
+      _listWithVenderPrefix(propName).forEach((name) =>
+          style.setProperty(name, value));
     }
   }
 }
 
-getCenterPoint(Element e) {
+List<String> _listWithVenderPrefix(String propName) =>
+  ['-webkit-$propName', '-moz-$propName', '-ms-$propName',
+   '-o-$propName', propName];
+
+_getCenterPoint(Element e) {
   final rects = e.getClientRects();
   return rects
     .map((r) => (r.topLeft + r.bottomRight) * 0.5)
     .fold(new Point(0.0, 0.0), (prev, p) => prev + p) * (1 / rects.length);
 }
-getTopLeftPoint(Element e) {
+_getTopLeftPoint(Element e) {
   final rects = e.getClientRects();
   return rects
     .map((r) => r.topLeft)
