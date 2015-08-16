c3b816a6dd13a7104ccc6f2bca923d8eba8fb9ee
Keiichiro Ui <keiichiro.ui@gmail.com>
2015-08-16 23:12:18 +0900

    Simplify tool/gh-pages.dart

--- tool/gh-pages.dart
+++ tool/gh-pages.dart
@@ -1,15 +1,9 @@
 #!/usr/bin/env dart
 
-import 'dart:io';
-import 'package:path/path.dart' as path;
 import 'package:ghpages_generator/ghpages_generator.dart' as gh;
 
-String PROJECT_DIR =
-    path.absolute(
-        path.dirname(path.dirname(Platform.script.toFilePath())));
-
 main() {
-  new gh.Generator(rootDir: PROJECT_DIR)
+  new gh.Generator()
     ..withExamples = true
     ..withIndexGeneration = true
     ..generate(doCustomTask: (workDir) {
